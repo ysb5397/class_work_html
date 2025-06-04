df65d28ce5a6a9e8e0ccdeb4d1fc3bb924448e81
Yangsb <ysb5397@gmail.com>
2025-06-04 16:39:28 +0900

    사이트 만들기

---
+++ tenco_online/js/main.js
@@ -0,0 +1,14 @@
+var cardNums;
+var currentPos;
+
+function clickLeft() {
+    currentPos = document.getElementsByClassName("class--list").item(0).id;
+    
+    // if (currentPos != cardNums) {
+
+    // }
+}
+
+function clickRight() {
+
+}
